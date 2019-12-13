2be7d059b94f914e5936c8d4b7523d305adfb0bd
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2019-12-13 06:34:23 -0600

    working,

--- react/homework4/FontChooser.js
+++ react/homework4/FontChooser.js
@@ -1,64 +1,75 @@
 class FontChooser extends React.Component {
 
     constructor(props) {
-	super(props);
-	this.state = { 
-			oculto : true,
-			fontsize: parseInt(this.props.size),
-			bold : (this.props.bold === 'true')
-		};
-    }
+    super(props);
+    this.state = { 
+            oculto : true,
+            fontsize: parseInt(this.props.size),
+            bold : (this.props.bold === 'true')
+        };
+    } //constructor
     
-    resetvalues(){
-	        this.setState(
-	        	{fontsize: parseInt(this.props.size)}
-	        );    
-
+    resetFontsize(){
+        this.setState(
+            {fontsize: parseInt(this.props.size)}
+        );    
     }
 
     toggleBold() {
-         this.setState( { bold : !this.state.bold } );
+        this.setState( { bold : !this.state.bold } );
     }
 
     toggle() {
         this.setState(
-        	{oculto: !this.state.oculto}
+            {oculto: !this.state.oculto}
         );
     }
 
     increase() {
-    	if (this.state.fontsize < this.props.max) {
-	        this.setState(
-	        	{fontsize: this.state.fontsize+1}
-	        );    
-        }
+        if (this.state.fontsize < parseInt(this.props.max)) {
+            this.setState(
+                {
+                    fontsize: this.state.fontsize+1,
+                }
+            );    
+        } //if
     }
 
     decrease() {
-    	if (this.state.fontsize > this.props.min) {
-	        this.setState(
-	        	{fontsize: this.state.fontsize-1}
-	        );    
-        }
+        if (this.state.fontsize > parseInt(this.props.min)) {
+            this.setState(
+                {
+                    fontsize: this.state.fontsize-1,
+                }
+            );    
+        } // IF
     }
 
+    checkLimits() {
+        var answer = false;
+        if ((this.state.fontsize == parseInt(this.props.max)) || (this.state.fontsize == parseInt(this.props.min))) {
+            answer = true;
+        }
+        return answer
+    }
 
     render() {
-		var bold = this.state.bold ? 'bold' : 'normal' ;
-		return(
-		       <div>
-		       	   <div className={this.state.oculto ? 'hidden' : ''}>
-				       <input type="checkbox" id="boldCheckbox" onChange={this.toggleBold.bind(this)} checked={this.state.bold} />
+        var bold = this.state.bold ? 'bold' : 'normal' ;
+        var color = this.checkLimits() ? 'red' : 'black';
+        return(
+               <div>
+                   <div className={this.state.oculto ? 'hidden' : ''}>
+                       <input type="checkbox" id="boldCheckbox" onChange={this.toggleBold.bind(this)} checked={this.state.bold} />
 
-				       <button id="decreaseButton" onClick={this.decrease.bind(this)}>-</button>
-				       <span id="fontSizeSpan" onDoubleClick={this.resetvalues.bind(this)}>{this.state.fontsize}</span>
-				       <button id="increaseButton" onClick={this.increase.bind(this)}>+</button>
+                       <button id="decreaseButton" onClick={this.decrease.bind(this)}>-</button>
+                       <span id="fontSizeSpan" onDoubleClick={this.resetFontsize.bind(this)} style={{color: color}}>{this.state.fontsize}</span>
+                       <button id="increaseButton" onClick={this.increase.bind(this)}>+</button>
 
-			   	   </div>    
-			       <span id="textSpan" onClick={this.toggle.bind(this)} style={{fontSize: this.state.fontsize, fontWeight:bold}}>
-			       	{this.props.text}
-			       </span>
-		       </div>
-		);
+                   </div>    
+                   <span id="textSpan" onClick={this.toggle.bind(this)} style={{fontSize: this.state.fontsize, fontWeight:bold}}>
+                    {this.props.text}
+                   </span>
+               </div>
+        );
     }
 }
