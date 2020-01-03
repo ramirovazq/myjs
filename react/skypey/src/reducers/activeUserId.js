bd4c68d6515647e42f33a45ba15d03ba213313ea
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2020-01-03 11:39:08 -0600

    working with pushing, new messages

--- react/skypey/src/reducers/activeUserId.js
+++ react/skypey/src/reducers/activeUserId.js
@@ -4,8 +4,6 @@ export default function activeUserId(state = null, action) {
 		switch (action.type){
 			case SET_ACTIVE_USER_ID: 
 				return action.payload;
-
-
 			default:
 				return state;
 		}
