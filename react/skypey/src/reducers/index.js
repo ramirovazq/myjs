dffd2724bf565fd3376cb130eb8441034f915260
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2020-01-02 19:07:17 -0600

    until, chat form where it stores in state

--- react/skypey/src/reducers/index.js
+++ react/skypey/src/reducers/index.js
@@ -1,11 +1,15 @@
 import user from "./user";
 import contacts from "./contacts";
 import activeUserId from "./activeUserId";
+import messages from "./messages"
+import typing from "./typing";
 
 import {combineReducers} from "redux";
 
 export default combineReducers({
 	user,
 	contacts,
-	activeUserId
+	activeUserId,
+ 	messages,
+ 	typing
 });
